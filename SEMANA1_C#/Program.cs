3b938b82a67dc474c7bfe460c1d1fbf166b4b50b
justhinvalleclass@gmail.com <justhinvalleclass@gmail.com>
2025-09-02 15:31:51 -0500

    subiendo ejercicio_SEM1

---
+++ SEMANA1_C#/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEMANA1_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ejer1();
+            Console.ReadKey();
+        }
+        static void ejer1()
+        {
+            string nombre, carrera;
+
+            Console.Write("Ingrese su nombre: ");
+            nombre = Console.ReadLine();
+
+            Console.Write("Ingrese su Carrera: ");
+            carrera = Console.ReadLine();
+
+            Console.Write($"\n{nombre}, bienvenido a FA de {carrera}");
+
+
+        }
+        static void ejer2()
+        {
+
+        }
+        static void ejer3()
+        {
+
+        }
+        static void ejer4()
+        {
+
+        }
+        static void ejer5()
+        {
+
+        }
+        static void ejer6()
+        {
+
+        }
+
+    }
+}
