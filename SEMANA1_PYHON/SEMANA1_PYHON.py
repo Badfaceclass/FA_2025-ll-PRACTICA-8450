41c48f1eb41fd72954dd087ff39e3b40f1c91381
bueno <justhinvalleclass@gmail.com>
2025-09-09 15:40:44 -0500

    Ejercicio 3-5

--- SEMANA1_PYHON/SEMANA1_PYHON.py
+++ SEMANA1_PYHON/SEMANA1_PYHON.py
@@ -1,7 +1,35 @@
-﻿def ejer1():
+﻿import math
+
+
+def ejer1():
     nombre = input("Ingrese su nombreP: ")
     carrera = input("Ingrese su carrera: ")
 
     print(f"\n{nombre}, bienvenido a FA de {carrera}")
 
-ejer1()
+def ejer2():
+    print("\"Bueno\"")
+
+
+def ejer3():
+        x = int(input("Ingres el valor de x: "))
+        y= int(input("Ingres el valor de y: "))
+
+        print("Suma: ", (x + y))
+        print("Resta: ", (x - y))
+        print("Multiplicacion: ", (x * y))
+        print("Division: ", (x / y))
+
+
+import math
+def ejer4():
+    num = float(input("Ingrese un numero decimal:"))
+
+    print("Raiz 2: ", math.sqrt(num))
+    print("Redondeado: ", round(num,0))
+    print("Al cubo: ", math.pow(num,3))
+    print("Raiz 3: ", num**(1/3))
+
+ 
+
+ejer4()
